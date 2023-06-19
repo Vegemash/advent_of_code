9baaf83ff381bb3b164c5e0973984aada861072b
Vegemash <mrisaacjackson@gmail.com>
2023-06-19 23:07:42 +1000

    13 Distress_norri_norri solution

--- 13_Distress_norri_norri/src/lib.rs
+++ 13_Distress_norri_norri/src/lib.rs
@@ -1,17 +1,57 @@
 use std::cmp::Ordering;
+use std::fmt::Display;
+use std::iter::zip;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 enum PacketPart {
     List(Vec<PacketPart>),
     Integer(u32),
 }
 
-fn compare_packets(a: PacketPart, b: PacketPart) -> Ordering {
-    match (a, b) {
-        (PacketPart::Integer(a_int), PacketPart::Integer(b_int)) => a_int.cmp(&b_int),
-        (PacketPart::List(a_list), PacketPart::List(b_list)) =>,
-        _ => todo!(),
-        
+impl Display for PacketPart {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PacketPart::Integer(int) => write!(f, "{}", int),
+            PacketPart::List(list) => {
+                write!(
+                    f,
+                    "[{}]",
+                    list.iter()
+                        .enumerate()
+                        .map(|(i, s)| format!("{}{}", if i != 0 { "," } else { "" }, s))
+                        .collect::<String>()
+                )
+            }
+        }
+    }
+}
+
+impl Ord for PacketPart {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (PacketPart::Integer(a_int), PacketPart::Integer(b_int)) => a_int.cmp(&b_int),
+            (PacketPart::List(a_list), PacketPart::List(b_list)) => {
+                for (a_part, b_part) in zip(a_list, b_list).into_iter() {
+                    let ord = a_part.cmp(&b_part);
+                    if ord != Ordering::Equal {
+                        return ord;
+                    }
+                }
+                return a_list.len().cmp(&b_list.len());
+            }
+            (PacketPart::List(a_list), PacketPart::Integer(b_int)) => {
+                a_list.cmp(&vec![PacketPart::Integer(b_int.to_owned())])
+            }
+            (PacketPart::Integer(a_int), PacketPart::List(b_list)) => {
+                vec![PacketPart::Integer(a_int.to_owned())].cmp(&b_list)
+            }
+        }
+    }
+}
+
+impl PartialOrd for PacketPart {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(&other))
     }
 }
 
@@ -25,7 +65,6 @@ fn parse_packet_parts(input: &str) -> Vec<PacketPart> {
     }
 
     let mut items: Vec<PacketPart> = vec![];
-    println!("parsing packet {}", input);
 
     let mut index = 0;
     let mut char_buff: String = "".to_string();
@@ -39,7 +78,9 @@ fn parse_packet_parts(input: &str) -> Vec<PacketPart> {
                 index = matching_brace;
             }
             Some(',') | Some(']') => {
-                items.push(PacketPart::Integer(char_buff.parse::<u32>().unwrap()));
+                if char_buff.len() > 0 {
+                    items.push(PacketPart::Integer(char_buff.parse::<u32>().unwrap()));
+                }
                 char_buff = "".to_string();
             }
             Some(ch) => char_buff += &ch.to_string(),
@@ -75,11 +116,49 @@ fn get_matching_index(input: &str) -> usize {
 }
 
 pub fn process_part_1(input: &str) -> u32 {
-    !todo!()
+    input
+        .lines()
+        .filter(|line| line != &"")
+        .map(|line| parse_packet(line))
+        .collect::<Vec<PacketPart>>()
+        .chunks(2)
+        .enumerate()
+        .into_iter()
+        .map(|(i, packets)| {
+            if packets[0].cmp(&packets[1]) == Ordering::Less {
+                println!("adding index {}", i + 1);
+                i + 1
+            } else {
+                0
+            }
+        })
+        .sum::<usize>() as u32
 }
 
 pub fn process_part_2(input: &str) -> String {
-    !todo!()
+    let mut packets = input
+        .lines()
+        .filter(|line| line != &"")
+        .map(|line| parse_packet(line))
+        .collect::<Vec<PacketPart>>();
+    packets.push(parse_packet("[[2]]"));
+    packets.push(parse_packet("[[6]]"));
+    packets.sort();
+    for packet in packets.iter() {
+        println!("{}", packet);
+    }
+    let mut two = 0;
+    let mut six = 0;
+    for (i, packet) in packets.iter().enumerate() {
+        let repr = format!("{}", packet);
+        if repr == "[[2]]" {
+            two = i + 1
+        }
+        if repr == "[[6]]" {
+            six = i + 1
+        }
+    }
+    (two * six).to_string()
 }
 
 #[cfg(test)]
@@ -117,14 +196,32 @@ mod tests {
     #[rstest]
     #[case(PacketPart::Integer(2), PacketPart::Integer(6), Ordering::Less)]
     fn test_compare_packets(#[case] a: PacketPart, #[case] b: PacketPart, #[case] ord: Ordering) {
-        assert_eq!(compare_packets(a, b), ord)
+        assert_eq!(a.cmp(&b), ord)
+    }
+
+    #[rstest]
+    #[case("[9]", "[[8,7,6]]", Ordering::Greater)]
+    #[case("[1,1,3,1,1]", "[1,1,5,1,1]", Ordering::Less)]
+    #[case("[[[]]]", "[[]]", Ordering::Greater)]
+    #[case(
+        "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+        "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+        Ordering::Greater
+    )]
+
+    fn test_parse_and_compare_packet(
+        #[case] a_packet: &str,
+        #[case] b_packet: &str,
+        #[case] ord: Ordering,
+    ) {
+        assert_eq!(parse_packet(a_packet).cmp(&parse_packet(b_packet)), ord)
     }
     #[test]
-    #[ignore]
     fn process_part_1_works() {
         let result = process_part_1(include_str!("../data/test_input"));
         assert_eq!(result, 13);
     }
+
     #[test]
     #[ignore]
     fn process_part_2_works() {
